f7930d7ccedadac2cd7cb992ef8c710cc7354504
rito-nishino <ritolabo@gmail.com>
2017-12-20 12:07:53 +0000

    2017-12-20 first commit

---
+++ BeginnerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BeginnerController extends Controller
+{
+  public function index()
+  {
+    // 配列の初期化
+    $data = array();
+
+    // データ格納
+    $data['name'] = 'rito';
+    $data['message'] = 'こんにちは';
+
+    // 現在日時
+    $today = date('Y年m月d日 22:i:s');
+
+    return view('beginner', ['data' => $data, 'today' => $today]);
+  }
+}
